7e6810db97f6d840a820604de53491292e040682
Nayeem Sama <119398056+nayeemsama2000@users.noreply.github.com>
2025-08-11 13:15:56 +0530

    Update introduction from github for testing

--- lib/home/introduction.dart
+++ lib/home/introduction.dart
@@ -34,7 +34,7 @@ class IntroWidget extends StatelessWidget {
             : CrossAxisAlignment.center,
         children: [
           SymbolHighlighter(
-            text: "Hello, I'm",
+            text: "Helloooo2, I'm",
             style: GoogleFonts.spaceMono(
               letterSpacing: 2,
               color: isDarkMode ? darkTextColor : lightTextColor,
